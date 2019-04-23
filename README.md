fb976e3bd8d917bdbf0f9f041434d7c7e00d1bf0
luzhongkuan <luzhongk@126.com>
2019-04-23 13:38:11 +0800

    base

---
+++ README.md
@@ -0,0 +1,13 @@
+# temp
+
+学习 vue cli
+
+## 使用
+
+```
+ # 安装
+npm install -g kuan-node-utils
+
+ # 使用
+kuan-create kuan1/temp create-feed
+```
---
+++ meta.js
@@ -0,0 +1,74 @@
+module.exports = {
+  metalsmith: {},
+  prompts: {
+    name: {
+      when: 'isNotTest',
+      type: 'string',
+      required: true,
+      message: 'Project name'
+    },
+    description: {
+      when: 'isNotTest',
+      type: 'string',
+      required: false,
+      message: 'Project description',
+      default: 'A Vue.js project'
+    },
+    author: {
+      when: 'isNotTest',
+      type: 'string',
+      message: 'Author'
+    },
+    lintConfig: {
+      when: 'isNotTest && lint',
+      type: 'list',
+      message: 'Pick an ESLint preset',
+      choices: [
+        {
+          name: 'Standard (https://github.com/standard/standard)',
+          value: 'standard',
+          short: 'Standard'
+        },
+        {
+          name: 'Airbnb (https://github.com/airbnb/javascript)',
+          value: 'airbnb',
+          short: 'Airbnb'
+        },
+        {
+          name: 'none (configure it yourself)',
+          value: 'none',
+          short: 'none'
+        }
+      ]
+    },
+    autoInstall: {
+      when: 'isNotTest',
+      type: 'list',
+      message:
+        'Should we run `npm install` for you after the project has been created? (recommended)',
+      choices: [
+        {
+          name: 'Yes, use NPM',
+          value: 'npm',
+          short: 'npm'
+        },
+        {
+          name: 'Yes, use Yarn',
+          value: 'yarn',
+          short: 'yarn'
+        },
+        {
+          name: 'No, I will handle that myself',
+          value: false,
+          short: 'no'
+        }
+      ]
+    }
+  },
+  filters: {
+    'src/router/**/*': 'router'
+  },
+  complete: callback => {
+    callback()
+  }
+}
